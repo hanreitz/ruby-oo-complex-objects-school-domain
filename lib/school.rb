724e0ff14b7d8d01bbddd5c9cc7e224803fa85d9
Hannah Lynn Reitzel Rivera <hanreitz@gmail.com>
2020-11-25 13:01:02 -0500

    commit tree 65ee61397137d0d193cfad29bd6baeb5d47963f0

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,33 @@
 # code here!
+require 'pry'
+class School
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+    attr_accessor :roster
+
+    def add_student(name, grade)
+       # binding.pry
+        if @roster.has_key?(grade) == true
+            @roster[grade] << "#{name}"
+        else
+            @roster[grade] = []
+            @roster[grade] << "#{name}"
+        end
+        @roster
+    end
+
+    def grade(grade)
+        @roster[grade]
+    end
+
+    def sort
+        sorted_hash = {}
+        @roster.each do |grade, student_list|
+            sorted_hash[grade] = student_list.sort
+        end
+        sorted_hash
+    end
+end
